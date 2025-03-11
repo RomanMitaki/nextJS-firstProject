8635d5597f4829fedecae8c2aa1504a2aa968bd0
RomanMitaki <r.mitaki@yandex.ru>
2025-03-11 20:03:27 +0300

    Добавлены и настроены Stylelint, Prettier и ESLint, внесены корректировки в package.json

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,33 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+import { FlatCompat } from '@eslint/eslintrc';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
+	baseDirectory: __dirname
+}); 
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+	...compat.extends('next/core-web-vitals', 'next/typescript'),
+	{
+		rules: {
+			'semi': 'off',
+			'@/semi': [
+				'warn'
+			],
+			'@typescript-eslint/no-empty-interface': [
+				'error',
+				{
+					'allowSingleExtends': true
+				}
+			],
+			'comma-dangle': ['error', 'never'],
+			'quotes': ['error', 'single'],
+			'indent': ['error', 'tab']
+		}
+	}
 ];
 
 export default eslintConfig;
