a3e0934d27a53eab2c6d53d8daa95ff9fb153264
RomanMitaki <r.mitaki@yandex.ru>
2025-03-13 11:14:05 +0300

    Добавить компонент Htag для отображения заголовков
    Создан компонент Htag с поддержкой заголовков h1, h2 и h3. Добавлены соответствующие стили, типизация пропсов и подключение компонента на главной странице. Теперь можно использовать Htag для стилизованного отображения заголовков.

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,12 @@
-export default function Home() {
-	return <div>Test</div>;
+import React, { JSX } from 'react';
+import { Htag } from '@/components';
+
+export default function Home(): JSX.Element {
+	return (
+		<>
+			<Htag tag="h1">Текст</Htag>
+			<Htag tag="h2">Текст</Htag>
+			<Htag tag="h3">Текст</Htag>
+		</>
+	);
 }
